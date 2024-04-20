887cc1b775b96b7779312be7cc764623c444e2a5
has-s <auttenberg@icloud.com>
2024-04-20 18:46:17 +0300

    try style

---
+++ static/js/theme.js
@@ -0,0 +1,23 @@
+const toggleSwitch = document.querySelector('#theme-toggle');
+
+// Функция для переключения темы
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.body.classList.add('dark-theme');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        document.body.classList.remove('dark-theme');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+// Установка сохраненной темы
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+    document.body.classList.add(currentTheme === 'dark' ? 'dark-theme' : 'light-theme');
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme);
